514f8d708b5cb51c70a3d070b3298912c9298038
Ariel J <arieljakubowski@gmail.com>
2019-05-22 14:55:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 min_key = ""
 min_val = ""
-key_for_min_value.each do |key, value|
+name_hash.each do |key, value|
   min_key = key if min_key = ""
   min_val = key if min_val = ""
   if value<min_val
